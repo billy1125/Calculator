574fd1cd7b57c04853fd1e27804b4c428ae15472
Cho-Hsun Lu <billy1125@gmail.com>
2024-04-15 14:28:14 +0800

    2024.04.15 改成類別與物件

--- Form1.cs
+++ Form1.cs
@@ -17,8 +17,7 @@ namespace Calculator
             InitializeComponent();
         }
 
-        // 全域變數
-        float firstNumber, secondNumber; // firstNumber 儲存第一個數字，secondNumber 儲存第二個數字
+        Calculate calculate = new Calculate(); // 建立計算機物件
         int operators = -1; // 記錄選擇哪一種運算符號？0:加、1:減、2:乘、3:除、-1:重新設定
 
         void Add_Number(string _number)
@@ -32,7 +31,7 @@ namespace Calculator
 
         private void Select_Operator(int _operator)
         {
-            firstNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第一個數字的全域變數
+            calculate.firstNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第一個數字的全域變數
             txtNumber.Text = "0"; //重新將輸入文字框重新設定為0
             operators = _operator; //選擇「加」號
         }
@@ -109,38 +108,36 @@ namespace Calculator
         private void btnEqual_Click(object sender, EventArgs e)
         {
             float finalResults = 0f; //宣告最後計算結果變數，區域變數
-            secondNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第二個數字的全域變數
+            calculate.secondNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第二個數字的全域變數
 
             //依照四則運算符號的選擇，進行加減乘除
             switch (operators)
             {
                 case 0:
-                    finalResults = firstNumber + secondNumber;
+                    finalResults = calculate.Add();
                     break;
                 case 1:
-                    finalResults = firstNumber - secondNumber;
+                    finalResults = calculate.Subtract();
                     break;
                 case 2:
-                    finalResults = firstNumber * secondNumber;
+                    finalResults = calculate.Multiply();
                     break;
                 case 3:
-                    finalResults = firstNumber / secondNumber;
+                    finalResults = calculate.Divide();
                     break;
             }
 
             txtNumber.Text = string.Format("{0:0.##########}", finalResults); //在輸入文字框中，顯示最後計算結果，並且轉換成格式化的字串內容
 
             //重置所有全域變數
-            firstNumber = 0f;
-            secondNumber = 0f;
+            calculate.Reset();
             operators = -1;
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
             txtNumber.Text = "0";
-            firstNumber = 0f;
-            secondNumber = 0f;
+            calculate.Reset();
             operators = -1;
         }
 
